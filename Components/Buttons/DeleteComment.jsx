3cd58e9bc6132ed540b618a79f0520facbd0028c
Kasshabz <142917268+Kasshabz@users.noreply.github.com>
2024-03-10 13:14:26 +0000

    delete function working

--- Components/Buttons/DeleteComment.jsx
+++ Components/Buttons/DeleteComment.jsx
@@ -1,14 +1,19 @@
+import articlesApi from "../../Utils/bluedit-api";
 function DeleteComment(props) {
   const { setComments, comments, article_id } = props;
 
   const handleDelete = (e) => {
     console.log(e.target);
-    console.log(comments.comment_id);
+
     console.log("deleted");
-    setComments((currComments)=>{
-        return [...currComments.filter(comments=>comments.comment_id !== comments.comment_id)]
-    })
-   
+    setComments((currComments) => {
+      return [
+        ...currComments.filter(
+          (comment) => comment.comment_id !== comments.comment_id
+        ),
+      ];
+    });
+    articlesApi.delete(`/comments/${comments.comment_id}`);
   };
   return <button onClick={handleDelete}>DELETE COMMENT</button>;
 }
